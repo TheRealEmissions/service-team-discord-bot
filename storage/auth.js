16468cc5212b48777952964f994945e910210bfc
Emissions <unitedrealm@hotmail.com>
2020-01-05 14:41:34 +0000

    Updated .gitignore

---
+++ storage/auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+   token: "NTg0NTEzNjE3OTYyMDc0MTMy.XhD1rA.RxyDNKkLQ46bkVhpEMvYFlfbAcA",
+   mongoUrl:
+      "mongodb+srv://user:Hd5V1v3UiOhBMS3S@emissions-fmfww.mongodb.net/tstb?retryWrites=true&w=majority"
+};
